8e00f4ab0fde5694cbf9699a12c799af5097f753
D Chris Cothern <63518372+dchriscothern@users.noreply.github.com>
2021-08-09 16:38:15 -0500

    Basic Template for Shiny

---
+++ ShinyAppTemplate.R
@@ -0,0 +1,8 @@
+library(shiny)
+ui <- fluidPage("Player Dashboard")
+server<- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
+
+# Original template to start all shiny apps, from Shiny R studio
+# https://shiny.rstudio.com/tutorial/ 
