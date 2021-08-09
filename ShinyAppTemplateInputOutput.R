c8b38b6c7986594f9bf1cc195b5f9f0d5957272f
dchriscothern <pristinehealth@yahoo.com>
2021-08-09 17:14:43 -0500

    Fixed space error

--- ShinyAppTemplateInputOutput.R
+++ ShinyAppTemplateInputOutput.R
@@ -1,7 +1,7 @@
 library(shiny)
-ui <- fluidPage(slider Input(inputId = "num",
-label = "Choose a number",
-value = 25, min = 1, max = 100)
+ui <- fluidPage(sliderInput(inputId = "num",
+          label = "Choose a number",
+          value = 25, min = 1, max = 100)
 )
 server<- function(input, output) {}
 
